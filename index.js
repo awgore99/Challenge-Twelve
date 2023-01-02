f02dd267ea35404daa712f7ad608a0d1baa06c2c
awgore99 <awgore99@gmail.com>
2023-01-02 11:25:40 -0600

    Added command prompt menu

--- index.js
+++ index.js
@@ -7,3 +7,28 @@ const db = mysql.createConnection({
     database: 'employee_db'
 },
 );
+
+function init(){
+    inquirer.prompt({
+        type: "list",
+        message: "Select one of the options below: ",
+        choices: [
+            "View All Departments",
+            "View All Employees",
+            "View All Roles",
+            "Add A Department",
+            "Add An Employee",
+            "Add A Role",
+            "Update An Employee's Role",
+            "Exit"
+        ],
+        name: "selection"
+    })
+    .then(answers => {
+        switch(answers.selection) {
+
+        }
+    })
+}
+
+init()
